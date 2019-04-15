1be5e7e7c194339f06a4e51f2e5d9667640133c2
simonequarticelli <quarti87@gmail.com>
2019-04-16 00:35:40 +0200

    progetto finito

--- pswgen.js
+++ pswgen.js
@@ -2,4 +2,6 @@ var nome = prompt ('inserisci il tuo nome');
 alert ('Ciao '+nome);
 var cognome = prompt('inserisci il tuo cognome');
 var colore = prompt('inserisci il tuo colore preferito');
+alert('Grazie');
 document.writeln(nome+cognome+colore+'19');
+console.log(nome+cognome+colore+'19');
