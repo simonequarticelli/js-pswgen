6f1ea0f7e23bff829e17d7ca18b75ff676b82eb6
simonequarticelli <quarti87@gmail.com>
2019-04-15 23:19:10 +0200

    inizio progetto pswgen.js

---
+++ pswgen.js
@@ -0,0 +1,5 @@
+var nome = prompt ('inserisci il tuo nome');
+alert ('Ciao '+nome);
+var cognome = prompt('inserisci il tuo cognome');
+var colore = prompt('inserisci il tuo colore preferito');
+document.writeln(nome+cognome+colore+'19');
